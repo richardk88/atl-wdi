659e36dd5cf5dac57a312e5e2083cce715139863
richardk88 <richardk88@hotmail.com>
2017-07-06 16:46:39 -0400

    answered functions-lab questions

--- labs/unit_01/javaScript/js-functions/functions-lab-starter.js
+++ labs/unit_01/javaScript/js-functions/functions-lab-starter.js
@@ -1,52 +1,87 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(x, y) {
+	if (y > x) {
+		return y;
+	} else {
+		return x;
+	}
 }
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(x, y, z) {
+	if (z > y && z > x) {
+		return z;
+	} else if (y > x && y > z) {
+		return  y;
+	} else if (x > y && x > z) {
+		return x;
+	}
 }
 
 // Question 3
-function isCharacterAVowel() {
-
+function isCharacterAVowel(word) {
+	var vowels = ['a', 'e', 'i', 'o', 'u'];
+	if (vowels.indexOf(word) !== -1) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 // Question 4
-function sumArray() {
- 
+function sumArray(array) {
+	var sum = 0;
+	for (var i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	return sum;
 }
 
 
 // Question 4
-function multiplyArray() {
-
+function multiplyArray(array) {
+	var product = [0];
+	for (var i = 1; i < array.length; i++) {
+		product *= array[i];
+	}
+	return product;
 }
 
 
 // Question 5
 var numberOfArguments = function(){
-  
+	return arguments.length;
 }
 
 
 
 // Question 6
-var reverseString = function (){
-  
+var reverseString = function (string){
+	return string.split('').reverse().join('');
 };
 
 
 // Question 7
-function findLongestWord () {
-  
+function findLongestWord (array) {
+  var longestWord = "";
+  for (var i = 0; i < array.length; i++) {
+  	if (array[i].length > longestWord.length) {
+  		longestWord = array[i];
+  	}
+  }
+  return longestWord;
 }
 
 
 // Question 8
-function filterLongWords () {
-  
+function filterLongWords (array, j) {
+	var newArray = [];
+	for (var i = 0; i < array.length; i++) {
+		if (array[i].length > j) {
+			newArray.push(array[i]);
+		}
+	}
+	return newArray;
 }
 
 
