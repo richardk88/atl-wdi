ff83b02a17f39102dd3f2a752242c8c980198386
richardk88 <richardk88@hotmail.com>
2017-07-19 17:24:13 -0400

    completed GET routes

--- homework/unit_02/express/pizza-express/app.js
+++ homework/unit_02/express/pizza-express/app.js
@@ -3,6 +3,19 @@ const hbs = require('hbs');
 
 const app = express();
 
+app.get('/', (req, res) => {
+	res.send('Welcome to Pizza Express!');
+})
+
+app.get('/topping/:type', (req, res) => {
+	res.send(`${req.param.type} pizza! Good choice.`);
+})
+
+app.get('/order/:amount/:pizzaSize', (req, res) => {
+	res.send(`Your order for ${req.params.amount} ${req.params.pizzaSize} pizzas will be ready in 1 minute!`);
+});
+
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
