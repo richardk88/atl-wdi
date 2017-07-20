98616f569087a6b5dcce6954efb830a79dc0cb47
richardk88 <richardk88@hotmail.com>
2017-07-20 19:55:46 -0400

    finished hw

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,40 @@
 //your code below
+const express = require('express');
+const router = express.Router();
+const pirates = require('../models/pirates.js')
+
+//index
+router.get('/', (req,res) => {
+    res.render('index', {
+        data: pirates
+    })
+})
+//new
+router.get('/new', (req,res) => {
+    res.render('new');
+})
+
+//show
+router.get('/:id', (req,res) => {
+    const id = req.params.id;
+    res.render('show', {
+    	data: pirates[id]
+    });
+})
+
+
+//post
+router.post('/', (req,res) => {
+    console.log(req.body);
+    const newPirate = {
+    	name: req.body.name,
+    	birthplace: req.body.birthplace,
+    	death_year: req.body.death_year,
+    	base: req.body.base,
+    	nickname: req.body.nickname
+    };
+    pirates.push(newPirate);
+    res.redirect('/pirates') 
+});
+
+module.exports = router;
--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,28 @@
 //your code here
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'hbs');
+
+
+//controllers for /pirates resource
+const pirateController = require('./controllers/pirates.js');
+app.use("/pirates", pirateController);
+
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`catch me at port ${port}`);
+});
