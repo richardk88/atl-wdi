e618e4301b7858c5d08c55c8cfa2a28e3829242f
richardk88 <richardk88@hotmail.com>
2017-08-03 16:42:23 -0400

    deploing with creat-react-app

--- labs/unit_03/react/pizza-express/src/App.js
+++ labs/unit_03/react/pizza-express/src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import Location from './components/Location';
+import SplashImage from './components/SplashImage';
+import Navbar from './components/Navbar';
+import Description from './components/Description';
+import MenuList from './components/MenuList';
+import LocationList from './components/LocationList';
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Place your components in here!</h1>
+        <Navbar />
+        <SplashImage />
+        <Description />
+        <MenuList />
+        <LocationList />
+        <Contact />
       </div>
     );
   }
