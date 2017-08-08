73a91e68fc8c4c516f0dbb40969f59693102d207
richardk88 <richardk88@hotmail.com>
2017-08-08 13:01:43 -0400

    attempted hw

--- homework/unit_03/react/bootflix/src/App.js
+++ homework/unit_03/react/bootflix/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './components/Header';
 import Search from './components/Search';
 import Movie from './components/Movie';
@@ -8,17 +9,41 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
+      titleToSearch: '',
+      idToSearch: '',
       movie: example
     }
   }
 
   //Update these methods to make axios calls to OMDB and update this.state.movie with the response from the server
   _searchByTitle = () => {
-    console.log("Search by Title");
+    axios.get(`http://www.omdbapi.com/?apikey=20c45c8e&t=${this.state.titleToSearch}`)
+      .then( (movie) => {
+        this.setState({movie: movie.data});
+        console.log(movie);
+      })
+      .catch( (error) => {
+        console.log(error);
+      })
   }
 
   _searchById = () => {
-    console.log("Search by ID");
+    axios.get(`http://www.omdbapi.com/?apikey=20c45c8e&i=${this.state.idToSearch}`)
+      .then( (movie) => {
+        this.setState({movie: movie.data});
+        console.log(movie);
+      })
+      .catch( (error) => {
+        console.log(error);
+      })
+  }
+
+  _handleTitleChange = (event) => {
+    this.setState({titleToSearch: event.target.value})
+  }
+
+  _handleIdChange = (event) => {
+    this.setState({idToSearch: event.target.value})
   }
 
   //Pass _searchByTitle, _searchById, and this.state.movie to it's appropriate child components.
@@ -26,8 +51,13 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Search />
-        <Movie />
+        <Search searchByTitle={this._searchByTitle} 
+          searchById={this._searchById}
+          titleToSearch={this.state.titleToSearch}
+          idToSearch={this.state.idToSearch}
+          handleTitleChange={this._handleTitleChange}
+          handleIdChange={this._handleIdChange} />
+        <Movie movie={this.state.movie} />
       </div>
     );
   }
