b808fa02c4cc0b3b4d5a6042f3ef9986ed25e1f1
richardk88 <richardk88@hotmail.com>
2017-08-17 18:42:30 -0400

    Commit: CARMEN SANDIEGO - I found Carmen Sandiego

--- homework/sql/carmen-sandiego/starter-code/find_carmen.sql
+++ homework/sql/carmen-sandiego/starter-code/find_carmen.sql
@@ -5,16 +5,17 @@ Clue #2: SELECT language FROM countrylanguage WHERE countrycode = 'VAT';
         ANSWER: Italian
 
 Clue #3: SELECT * FROM countrylanguage WHERE language = 'Italian' AND percentage = 100;
-        ANSWER: SMR
+        ANSWER: San Marino
 
-Clue #4: SELECT * FROM country WHERE name = 'Serravelle';
-        ANSWER: Serravelle
+Clue #4: SELECT * FROM country WHERE name = 'Serravalle';
+        ANSWER: Serravalle
 
-Clue #5: 
-        ANSWER:
+Clue #5: SELECT name FROM city WHERE name LIKE '%Serra%';
+        ANSWER: Serra
 
-Clue #6: 
-        ANSWER:
+Clue #6: SELECT capital FROM country WHERE code = 'BRA';
+         SELECT name FROM city WHERE id = 211;
+        ANSWER: Brasï¿½lia
 
-Clue #7: 
-        ANSWER:          
+Clue #7: SELECT * FROM city WHERE population = 91084;
+        ANSWER: Santa Monica
