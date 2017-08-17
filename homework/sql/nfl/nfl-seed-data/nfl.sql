994973c8740de7e7d7f8cc83cdb6774e85afae57
richardk88 <richardk88@hotmail.com>
2017-08-17 19:02:50 -0400

    Commit: NFL - queried some NFL stuff

---
+++ homework/sql/nfl/nfl-seed-data/nfl.sql
@@ -0,0 +1,4 @@
+1. SELECT name FROM teams;
+2. SELECT stadium,head_coach  FROM teams;
+3. SELECT head_coach FROM teams WHERE division = 'South' AND conference = 'AFC';
+4. SELECT COUNT(name) FROM players;
