71728f469408c47c2a0cfa89a30e5290aa27f958
richardk88 <richardk88@hotmail.com>
2017-08-17 19:21:08 -0400

    Commit: NFL - wow there's a lot of nfl data

--- homework/sql/nfl/nfl-seed-data/nfl.sql
+++ homework/sql/nfl/nfl-seed-data/nfl.sql
@@ -1,4 +1,8 @@
-1. SELECT name FROM teams;
-2. SELECT stadium,head_coach  FROM teams;
-3. SELECT head_coach FROM teams WHERE division = 'South' AND conference = 'AFC';
-4. SELECT COUNT(name) FROM players;
+SELECT name FROM teams;
+SELECT stadium,head_coach  FROM teams;
+SELECT head_coach FROM teams WHERE division = 'South' AND conference = 'AFC';
+SELECT COUNT(name) FROM players;
+SELECT name, head_coach FROM teams WHERE (division = 'North' AND conference = 'NFC') OR (division = 'AFC' AND conference = 'AFC');
+SELECT * FROM players ORDER BY salary ASC LIMIT 50;
+SELECT AVG(salary) FROM players;
+SELECT name, position FROM players WHERE salary > 10000000;
