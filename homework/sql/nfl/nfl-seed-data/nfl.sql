5826e070cf6eb5922c244df0b1c8892b7d57c090
richardk88 <richardk88@hotmail.com>
2017-08-17 19:34:24 -0400

    Commit: NFL - Finished the nfl queries

--- homework/sql/nfl/nfl-seed-data/nfl.sql
+++ homework/sql/nfl/nfl-seed-data/nfl.sql
@@ -6,3 +6,9 @@ SELECT name, head_coach FROM teams WHERE (division = 'North' AND conference = 'N
 SELECT * FROM players ORDER BY salary ASC LIMIT 50;
 SELECT AVG(salary) FROM players;
 SELECT name, position FROM players WHERE salary > 10000000;
+SELECT * FROM players ORDER BY salary DESC;
+SELECT * FROM players ORDER BY salary ASC LIMIT 100;
+SELECT AVG(salary) FROM players WHERE position = 'DE';
+SELECT * FROM players WHERE team_id = 1;
+SELECT AVG(salary) FROM players WHERE team_id = 18;
+SELECT name FROM players WHERE team_id = 23 ORDER BY salary ASC LIMIT 1;
