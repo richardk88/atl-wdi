42399c952d4cd368a95449a3d1b9bbf468085fc5
richardk88 <richardk88@hotmail.com>
2017-08-17 18:02:31 -0400

    answered clue 1-4

---
+++ homework/sql/carmen-sandiego/starter-code/find_carmen.sql
@@ -0,0 +1,20 @@
+Clue #1: SELECT name, population FROM country WHERE region = 'Southern Europe'; 
+        ANSWER: Holy See (Vatican City State)
+
+Clue #2: SELECT language FROM countrylanguage WHERE countrycode = 'VAT';
+        ANSWER: Italian
+
+Clue #3: SELECT * FROM countrylanguage WHERE language = 'Italian' AND percentage = 100;
+        ANSWER: SMR
+
+Clue #4: SELECT * FROM country WHERE name = 'Serravelle';
+        ANSWER: Serravelle
+
+Clue #5: 
+        ANSWER:
+
+Clue #6: 
+        ANSWER:
+
+Clue #7: 
+        ANSWER:          
