82d32f5b386de5d7a2496cc779155d89c792f2c3
richardk88 <richardk88@hotmail.com>
2017-08-17 23:58:08 -0400

    Commit: REALTY - Realty seed file created and db seeded

---
+++ homework/sql/realty/realty_seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO apartments
+( apt_num, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ( '12', 2, 1, '123 Sesame Street, New York, NY', 'Bert & Earnie', 'true', 1111, 1111121);
+
+INSERT INTO apartments
+( apt_num, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ( '10', 4, 1.5, '111 Broadway, New York, NY', 'DFW', 'false', 2045, 1211121);
+
+INSERT INTO apartments
+( apt_num, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ( '20', 6, 2, '444 Lafayette, New York, NY', 'John Steinbeck', 'true', 3020, 1011121);
+
+INSERT INTO apartments
+( apt_num, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ( '33', 3, 3, '3333 Broadway, New York, NY', 'Neil Gaiman', 'true', 3333, 3333333);
+
+INSERT INTO apartments
+( apt_num, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ( '45', 9, 4.5, '5 Broadway, New York, NY', 'Brian K. Vaughn', 'true', 9000, 911121);
+
+INSERT INTO apartments
+( apt_num, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ( 777, 7, 7, '7777 Broadway, New York, NY', 'James Hamblin', 'true', 7777, 77777777);
+
+INSERT INTO apartments
+( apt_num, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) VALUES ('240', 2, 1, '432 Macy Summit 18865-1026 South Carolina' , 'Gaylord Bashirian Sr.' , 'true', 4469, 5252589);
