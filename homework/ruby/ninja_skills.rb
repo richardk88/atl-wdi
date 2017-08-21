3b45791f205b86dd73c0463743cae78727d5d9c0
richardk88 <richardk88@hotmail.com>
2017-08-21 17:38:18 -0400

    4. String skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -14,9 +14,18 @@
 
 
 #Challenge 3:
-d = Fixnum
-puts d
-e = Float
-puts e 
-d = e
-puts d
+# d = Fixnum
+# puts d
+# e = Float
+# puts e 
+# d = e
+# puts d
+
+#Challenge 4:
+christine = "Hi, I'm Christine"
+likesTo = "long walks on the beach, kinda."
+tims = christine
+puts tims + " and I like " + likesTo
+g = "2"
+gToNumber = 2
+puts g.to_i + gToNumber
