2f16bf5c62158807380de9122b6ec07a1ae8bc26
richardk88 <richardk88@hotmail.com>
2017-08-21 23:11:28 -0400

    completed challenge 9

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -73,24 +73,33 @@
 
 
 #Challenge 8:
-firstRange = (1..10).to_a
-secondRange = (1...1000).to_a
+# firstRange = (1..10).to_a
+# secondRange = (1...1000).to_a
 
-for number in firstRange do
-    p number.to_s
-end
+# for number in firstRange do
+#     p number.to_s
+# end
 
-x = 0
-while x < 50 do
-    puts secondRange[x]
-    x += 1
-end
+# x = 0
+# while x < 50 do
+#     puts secondRange[x]
+#     x += 1
+# end
 
-newArray = []
-firstRange.each_with_index do |number, index|
-    if index.even?
-        newArray << (number * 2)
+# newArray = []
+# firstRange.each_with_index do |number, index|
+#     if index.even?
+#         newArray << (number * 2)
+#     end
+# end
+# puts newArray
+
+
+#Challenge 9:
+sum = 0
+for i in (1...1000) do
+    if i % 3 == 0 or i % 5 == 0
+        sum += 1
     end
 end
-puts newArray
-
+puts sum
